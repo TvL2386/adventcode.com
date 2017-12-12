f199b954f0abbf11a17be3a2a2428fb83f033c06
Tom van Leeuwen <tom.van.leeuwen@artofautomation.net>
2017-12-12 18:28:52 +0100

    added part2.rb

---
+++ 2017/day08/part2.rb
@@ -0,0 +1,61 @@
+module Day08
+  class InputParser
+    attr_reader :heighest_value
+
+    def initialize(file)
+      @input_lines = File.read(file).split("\n")
+    end
+
+    def execute_code
+      @variable_store = Hash.new { |h,k| h[k] = 0 }
+      @heighest_value = 0
+
+      @input_lines.each do |line|
+        ruby = to_ruby(line)
+        eval ruby
+
+        if @heighest_value < max_value
+          @heighest_value = max_value
+        end
+      end
+    end
+
+    def max_value
+      @variable_store.values.max
+    end
+
+    def to_ruby(line)
+      tokens = line.split(' ')
+
+      new_tokens = []
+      new_tokens << to_var(tokens.shift)
+      new_tokens << to_oper(tokens.shift)
+      new_tokens << tokens.shift
+      new_tokens << tokens.shift
+      new_tokens << to_var(tokens.shift)
+      new_tokens << tokens.shift
+      new_tokens << tokens.shift
+      new_tokens.join(' ')
+    end
+
+    def to_var(string)
+      "@variable_store[:#{string}]"
+    end
+
+    def to_oper(string)
+      case string
+        when 'dec'
+          '-='
+        when 'inc'
+          '+='
+        else
+          raise ArgumentError, "Unknown operator #{string}"
+      end
+    end
+  end
+end
+
+parser = Day08::InputParser.new('input')
+parser.execute_code
+puts parser.max_value
+puts parser.heighest_value
